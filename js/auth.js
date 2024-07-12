9369db5ddab34ffe1eb20e1548042be726610039
Ahmed <mman7895@gmail.com>
2024-07-12 19:45:15 +0100

    changes 2.7

--- js/auth.js
+++ js/auth.js
@@ -97,7 +97,7 @@ const addCommand = async (userName, userphone, userAdress, email) => {
       const targetDocRef = doc(firestore, 'commands', userName+"_"+Date.now());
       await setDoc(targetDocRef, data);
       await deleteDoc(sourceDocRef);
-    //   window.location.href = "./thankyou.html";
+      window.location.href = "./thankyou.html";
     } catch (error) {
       console.error('Error copying document: ', error);
     }
